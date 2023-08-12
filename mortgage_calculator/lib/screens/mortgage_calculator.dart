566cf47e29db2448aad7c4e34da1331e12d0c9dc
Walt <walt.c0des20@gmail.com>
2023-08-12 15:55:06 -0600

    initial commit

---
+++ mortgage_calculator/lib/screens/mortgage_calculator.dart
@@ -0,0 +1,153 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import '/auth/secrets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MortgageCalculator extends StatefulWidget {
+  const MortgageCalculator({super.key});
+  @override
+  MortgageCalculatorState createState() => MortgageCalculatorState();
+}
+
+class MortgageCalculatorState extends State<MortgageCalculator> {
+  double rate30 = 0.0;
+  double rate15 = 0.0;
+  String? selectedMortgageType; // defualt value
+  String? selectedMortgageTerm;
+  double monthlyMortgage = 0.0;
+  double interestRate = 6.665;
+  TextEditingController loanAmountController = TextEditingController();
+  final _dropdownFormKey = GlobalKey<FormState>();
+  List<DropdownMenuItem<String>> get mortgageTypeItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      const DropdownMenuItem(
+          value: "Conventional", child: Text("Conventional")),
+      const DropdownMenuItem(value: "FHA", child: Text("FHA")),
+      const DropdownMenuItem(
+        value: "VA",
+        child: Text("VA"),
+      )
+    ];
+    return menuItems;
+  }
+
+  List<DropdownMenuItem<String>> get mortgageTermItems {
+    List<DropdownMenuItem<String>> menuItems = [
+      const DropdownMenuItem(value: "15 Year", child: Text("15 Year")),
+      const DropdownMenuItem(value: "30 Year", child: Text("30 Year"))
+    ];
+    return menuItems;
+  }
+
+  double calculateMortgage(
+      double loanAmout, double interestRate, int loanTermInMonths) {
+    double monthlyInterestRate = interestRate / 12 / 100;
+    double numerator = loanAmout *
+        monthlyInterestRate *
+        pow(1 + monthlyInterestRate, loanTermInMonths);
+    double denominator = pow(1 + monthlyInterestRate, loanTermInMonths) - 1;
+    return numerator / denominator;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mortgage Calculator')),
+      body: Center(
+        child: Column(
+          key: _dropdownFormKey,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: TextFormField(
+                    controller: loanAmountController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(labelText: 'Loan Amount'),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        filled: true,
+                        fillColor: Colors.blueAccent,
+                      ),
+                      validator: (value) =>
+                          value == null ? "Select Mortgage Type" : null,
+                      dropdownColor: Colors.blueAccent,
+                      value: selectedMortgageType,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedMortgageType = newValue!;
+                        });
+                      },
+                      items: mortgageTypeItems),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.blue, width: 2),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      filled: true,
+                      fillColor: Colors.blueAccent,
+                    ),
+                    validator: (value) =>
+                        value == null ? 'Select Loan Term' : null,
+                    dropdownColor: Colors.blueAccent,
+                    value: selectedMortgageTerm,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedMortgageTerm = newValue!;
+                      });
+                    },
+                    items: mortgageTermItems,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {
+                      double loanAmount =
+                          double.parse(loanAmountController.text);
+                      int loanTermInMonths =
+                          selectedMortgageTerm == '15 Year' ? 15 * 12 : 30 * 12;
+                      monthlyMortgage = calculateMortgage(
+                          loanAmount, interestRate, loanTermInMonths);
+                      debugPrint('Monthly Payment: $monthlyMortgage');
+                      setState(() {});
+                    },
+                    child: const Text('Calculate')),
+                const SizedBox(height: 20),
+                Text(
+                  'Monthly Payment \$${monthlyMortgage.toStringAsFixed(2)}',
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
