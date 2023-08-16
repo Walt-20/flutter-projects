125e1b893825a3bd967da96520da3949ef1cc833
Walt <walt.c0des20@gmail.com>
2023-08-15 18:42:18 -0600

    commit

--- mortgage_calculator/lib/main.dart
+++ mortgage_calculator/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:fl_chart/fl_chart.dart';
 
+import 'screens/home_screen.dart';
 import 'screens/mortgage_calculator.dart';
 import 'screens/gross_yearly_income.dart';
 import 'shared_data.dart';
@@ -30,111 +30,6 @@ class MyApp extends StatelessWidget {
         '/mortgage_calculator': (context) => const MortgageCalculator(),
         '/gross_yearly_income': (context) => const GrossYearlyIncome(),
       },
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-        actions: [
-          Builder(
-            builder: (context) => IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-            ),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              title: const Text('Mortgage Calculator'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                Navigator.pushNamed(context, '/mortgage_calculator');
-              },
-            ),
-            ListTile(
-              title: const Text('Gross Yearly Income'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                Navigator.pushNamed(context, '/gross_yearly_income');
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Consumer<SharedData>(
-          builder: (context, sharedData, child) {
-            double monthlyMortgage = sharedData.monthlyMortgage;
-            double grossIncome = sharedData.grossIncome;
-            double monthlyGrossIncome = grossIncome / 12;
-            double debtToIncomeRatio =
-                (monthlyMortgage / monthlyGrossIncome) * 100;
-            double remainingPercentage = 100 - debtToIncomeRatio;
-            debugPrint("getting monthly mortgage $monthlyMortgage");
-            debugPrint("getting gross income $monthlyGrossIncome");
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AspectRatio(
-                  aspectRatio: 1.5,
-                  child: PieChart(
-                    PieChartData(
-                      sections: [
-                        PieChartSectionData(
-                          value: debtToIncomeRatio,
-                          color: Colors.blue,
-                          title: '${debtToIncomeRatio.toStringAsFixed(2)}%',
-                          radius: 50,
-                          titleStyle: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        PieChartSectionData(
-                          value: remainingPercentage,
-                          color: Colors.grey,
-                          title: '${remainingPercentage.toStringAsFixed(2)}%',
-                        ),
-                      ],
-                      sectionsSpace: 0,
-                      centerSpaceRadius: 30,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                    'Monthly Mortgage: \$${monthlyMortgage.toStringAsFixed(2)}'),
-                Text('Gross Income: \$${grossIncome.toStringAsFixed(2)}'),
-              ],
-            );
-          },
-        ),
-      ),
     );
   }
 }
---
+++ mortgage_calculator/lib/screens/home_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../shared_data.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        actions: [
+          Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Mortgage Calculator'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                Navigator.pushNamed(context, '/mortgage_calculator');
+              },
+            ),
+            ListTile(
+              title: const Text('Gross Yearly Income'),
+              onTap: () {
+                Navigator.pop(context); // Close the drawer
+                Navigator.pushNamed(context, '/gross_yearly_income');
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Consumer<SharedData>(
+          builder: (context, sharedData, child) {
+            double monthlyMortgage = sharedData.monthlyMortgage;
+            double grossIncome = sharedData.grossIncome;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Monthly Mortgage: \$${monthlyMortgage.toStringAsFixed(2)}',
+                  style: const TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Gross Monthly Income: \$${grossIncome.toStringAsFixed(2)}',
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
